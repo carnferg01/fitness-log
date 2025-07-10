5887a77f95cdaea5cedd19913921d8245da8b866
carnferg01 <carnferg01@gmail.com>
2025-07-10 11:41:07 +0100

    Created gear edit & delete. Now using bootstrap. Now using modulo html pages.

--- fitnesslog_app/urls.py
+++ fitnesslog_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # Gear
     path('gear/', views.gear_list, name='gear_list'),
     path('gear/add/', views.gear_add, name='gear_add'),
+    path('gear/<int:pk>/edit/', views.gear_edit, name='gear_edit'),
+    path('gear/<int:pk>/delete/', views.gear_delete, name='gear_delete'),
 ]
--- fitnesslog_app/views.py
+++ fitnesslog_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Gear
 from .forms import GearForm
@@ -20,3 +20,20 @@ def gear_add(request):
         form = GearForm()
     return render(request, 'gear_add.html', {'form': form})
 
+def gear_edit(request, pk):
+    gear_item = get_object_or_404(Gear, pk=pk)
+    if request.method == 'POST':
+        form = GearForm(request.POST, instance=gear_item)
+        if form.is_valid():
+            form.save()
+            return redirect('gear_list')  # change as needed
+    else:
+        form = GearForm(instance=gear_item)
+    return render(request, 'gear_edit.html', {'form': form, 'gear_item': gear_item})
+
+def gear_delete(request, pk):
+    gear_item = get_object_or_404(Gear, pk=pk)
+    if request.method == 'POST':
+        gear_item.delete()
+        return redirect('gear_list')  # change as needed
+    return render(request, 'confirm_delete.html', {'gear_item': gear_item})
